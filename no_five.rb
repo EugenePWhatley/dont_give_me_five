aab84cebc74947d2e40e956dd35e24e7cb5b980b
Eugene Whatley <ewhatley@thoughtworks.com>
2021-07-06 22:47:59 -0400

    Refactor to module instead of class

--- no_five.rb
+++ no_five.rb
@@ -1,6 +1,7 @@
-class NoFive
+module NoFive
   def count_range_without_fives(first, last)
-    Array(first..last).select {|num| num.to_s[-1] != "5"}.count
+    Array(first..last)
+      .select {|num| num.to_s[-1] != "5"}
+      .count
   end
-  
 end
--- spec/no_five_spec.rb
+++ spec/no_five_spec.rb
@@ -1,28 +1,25 @@
 require './no_five'
+include NoFive
 
 describe NoFive do
 
   it "should count how many numbers between 2 points" do
-    no_five = NoFive.new
-    actual = no_five.count_range_without_fives(1,4)
+    actual = NoFive.count_range_without_fives(1,4)
     expect(actual).to eq 4
   end
 
   it "should count how many numbers between 2 points not including 5" do
-    no_five = NoFive.new
-    actual = no_five.count_range_without_fives(1,9)
+    actual = NoFive.count_range_without_fives(1,9)
     expect(actual).to eq 8
   end
 
   it "should count how many numbers between 2 points not including 5 as last digit" do
-    no_five = NoFive.new
-    actual = no_five.count_range_without_fives(11,16)
+    actual = NoFive.count_range_without_fives(11,16)
     expect(actual).to eq 5
   end
 
   it "should count how many numbers between 2 points not including 5 as last digit where 2 numbers end in 5" do
-    no_five = NoFive.new
-    actual = no_five.count_range_without_fives(4,17)
+    actual = NoFive.count_range_without_fives(4,17)
     expect(actual).to eq 12
   end
 
