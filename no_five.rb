74aea14acc87a345e8977f82250275cf641be164
Eugene Whatley <ewhatley@thoughtworks.com>
2021-07-06 22:46:01 -0400

    Fix bug subtracting 1; Use example in README in test case

--- no_five.rb
+++ no_five.rb
@@ -1,6 +1,6 @@
 class NoFive
   def count_range_without_fives(first, last)
-    Array(first..last).select {|num| num.to_s[-1] != "5"}.count - 1
+    Array(first..last).select {|num| num.to_s[-1] != "5"}.count
   end
   
 end
--- spec/no_five_spec.rb
+++ spec/no_five_spec.rb
@@ -5,18 +5,25 @@ describe NoFive do
   it "should count how many numbers between 2 points" do
     no_five = NoFive.new
     actual = no_five.count_range_without_fives(1,4)
-    expect(actual).to eq 3
+    expect(actual).to eq 4
   end
 
   it "should count how many numbers between 2 points not including 5" do
     no_five = NoFive.new
-    actual = no_five.count_range_without_fives(1,6)
-    expect(actual).to eq 4
+    actual = no_five.count_range_without_fives(1,9)
+    expect(actual).to eq 8
   end
 
   it "should count how many numbers between 2 points not including 5 as last digit" do
     no_five = NoFive.new
     actual = no_five.count_range_without_fives(11,16)
-    expect(actual).to eq 4
+    expect(actual).to eq 5
   end
+
+  it "should count how many numbers between 2 points not including 5 as last digit where 2 numbers end in 5" do
+    no_five = NoFive.new
+    actual = no_five.count_range_without_fives(4,17)
+    expect(actual).to eq 12
+  end
+
 end
