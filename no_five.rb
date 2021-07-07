c85e638aaeb823986141ebf87d5bf3d3b2084caa
Eugene Whatley <ewhatley@thoughtworks.com>
2021-07-06 22:40:09 -0400

    Count range skipping 5

--- no_five.rb
+++ no_five.rb
@@ -1,6 +1,6 @@
 class NoFive
   def count_range_without_fives(first, last)
-    Array(first..last).length - 1
+    Array(first..last).select {|num| num != 5}.count - 1
   end
   
 end
--- spec/no_five_spec.rb
+++ spec/no_five_spec.rb
@@ -8,4 +8,9 @@ describe NoFive do
     expect(actual).to eq 3
   end
 
+  it "should count how many numbers between 2 points not including 5" do
+    no_five = NoFive.new
+    actual = no_five.count_range_without_fives(1,6)
+    expect(actual).to eq 4
+  end
 end
